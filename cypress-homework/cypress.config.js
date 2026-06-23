e8fb76b73f30eb8f7d97b1e829dc4e86e1b09f90
Serhii Kravchenko <abaddonvong@gmail.com>
2026-06-23 19:35:50 +0300

    Minor updates

--- cypress-homework/cypress.config.js
+++ cypress-homework/cypress.config.js
@@ -9,9 +9,9 @@ module.exports = defineConfig({
     },
       baseUrl: 'https://api.clickup.com/api/v2',
       env: {
-        validToken: 'pk_302572542_Y5JTCAMFUC6GRCO0LHGSGGJQWJGTN0UR',
+        validToken: 'pk_302572951_J1BQGT89EPXZE9UR13X0HATF7T5IETGH',
         invalidToken: 'pk_blablablaImInvalidSowhat',
-        teamId: '90121848771'
+        teamId: '90121849074'
       }
   },
 });
